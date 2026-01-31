077cb140adeaaf3657e6dab26623078af3c12465
latihan210 <yosepsolahudin200@gmail.com>
2026-02-01 02:18:40 +0700

    test

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -11,3 +11,6 @@
 <a href="/contact"
     class="rounded-md px-3 py-2 text-sm font-medium {{ request()->is('contact') ? $activeClass : $defaultClass }}"
     {{ request()->is('contact') ? 'aria-current=page' : '' }}>Contact</a>
+<a href="/login"
+    class="rounded-md px-3 py-2 text-sm font-medium {{ request()->is('login') ? $activeClass : $defaultClass }}"
+    {{ request()->is('login') ? 'aria-current=page' : '' }}>Login</a>
